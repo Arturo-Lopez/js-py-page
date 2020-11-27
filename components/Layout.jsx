84fa88f85582b99a9ee27318fffba336f4637659
Arturo-Lopez <jo.arturo.lopez.go@gmail.com>
2020-11-27 01:42:07 -0300

    going to the dark side

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
 const Layout = ({ children }) => {
+  const [dark, setDark] = useState(false);
+
   return (
-    <main>
+    <main className={dark ? "dark" : ""}>
       <header>
         <Image src="/logo.png" width="100%" height="100%" />
         <nav>
@@ -14,6 +16,15 @@ const Layout = ({ children }) => {
           <Link href="/contacto">
             <a>Contacto</a>
           </Link>
+          <label htmlFor="check-dark">
+            Dark
+            <input
+              type="checkbox"
+              id="check-dark"
+              checked={dark}
+              onChange={() => setDark((prev) => !prev)}
+            />
+          </label>
         </nav>
       </header>
       <div className="content">{children}</div>
